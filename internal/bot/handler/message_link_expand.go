a73e22514b3d1b4aff1bf1015834648e2bf06403
aqyuki <aqyuki@aqyuki.dev>
2024-07-07 20:09:03 +0900

    feat: メッセージ展開時に，リアクションの絵文字をEmbedのフィールドに追加 (#20)

--- internal/bot/handler/message_link_expand.go
+++ internal/bot/handler/message_link_expand.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 	"strings"
 	"time"
@@ -88,6 +89,30 @@ func (h *messageLinkExpandHandler) Expand(session *discordgo.Session, message *d
 		}
 	}
 
+	// メッセージにリアクションが有る場合は，Embedのフィールドに追加する．
+	var field *discordgo.MessageEmbedField
+	if len(msg.Reactions) > 0 {
+		field = &discordgo.MessageEmbedField{
+			Name:   "Reactions",
+			Value:  "",
+			Inline: true,
+		}
+		for _, reaction := range msg.Reactions {
+			var emoji string
+			if reaction.Emoji.ID != "" {
+				raw, err := session.GuildEmoji(channel.GuildID, reaction.Emoji.ID)
+				if err != nil {
+					h.logger.Errorf("failed to get emoji: %v", err)
+					continue
+				}
+				emoji = raw.MessageFormat()
+			} else {
+				emoji = reaction.Emoji.Name
+			}
+			field.Value = fmt.Sprintf("%s%s ", field.Value, emoji)
+		}
+	}
+
 	embed := &discordgo.MessageEmbed{
 		Image: image,
 		Author: &discordgo.MessageEmbedAuthor{
@@ -96,6 +121,7 @@ func (h *messageLinkExpandHandler) Expand(session *discordgo.Session, message *d
 		},
 		Color:       0x7fffff,
 		Description: msg.Content,
+		Fields:      fieldOrNil(field),
 		Timestamp:   msg.Timestamp.Format(time.RFC3339),
 		Footer: &discordgo.MessageEmbedFooter{
 			Text: channel.Name,
@@ -138,4 +164,11 @@ type message struct {
 	guild   string
 	channel string
 	message string
+}
+
+func fieldOrNil(field *discordgo.MessageEmbedField) []*discordgo.MessageEmbedField {
+	if field == nil {
+		return nil
+	}
+	return []*discordgo.MessageEmbedField{field}
 }
