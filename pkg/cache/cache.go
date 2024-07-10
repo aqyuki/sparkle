9dbace0e6a72da4a31e7c351044376d9056a418d
aqyuki <aqyuki@aqyuki.dev>
2024-07-10 21:25:58 +0900

    chore: 使用していない関数を削除

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/patrickmn/go-cache"
-	"github.com/samber/do"
 )
 
 var _ CacheStore = (*ImMemoryCacheStore)(nil)
@@ -17,12 +16,6 @@ type CacheStore interface {
 type ImMemoryCacheStore struct {
 	store           *cache.Cache
 	cacheExpiration time.Duration
-}
-
-func NewCacheStore(expiration, clean time.Duration) do.Provider[CacheStore] {
-	return func(i *do.Injector) (CacheStore, error) {
-		return NewImMemoryCacheStore(expiration, clean), nil
-	}
 }
 
 func NewImMemoryCacheStore(expiration, clean time.Duration) *ImMemoryCacheStore {
