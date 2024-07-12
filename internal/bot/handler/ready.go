818f370809e8adc0bb7429a8b381766725809895
aqyuki <aqyuki@aqyuki.dev>
2024-07-12 21:28:39 +0900

    feat!: インターフェイスの変更に伴い修正

--- internal/bot/handler/ready.go
+++ internal/bot/handler/ready.go
@@ -1,35 +1,24 @@
 package handler
 
 import (
-	"fmt"
-
-	"github.com/aqyuki/sparkle/internal/bot"
 	"github.com/aqyuki/sparkle/internal/information"
 	"github.com/bwmarrin/discordgo"
 	"go.uber.org/zap"
 )
 
-var _ bot.ReadyHandler = (*ReadyHandler)(nil)
-
 type ReadyHandler struct {
-	logger          *zap.SugaredLogger
-	versionResolver information.InformationProvider
+	logger *zap.SugaredLogger
+	info   *information.BotInformation
 }
 
-func NewReadyHandler(logger *zap.SugaredLogger, resolver information.InformationProvider) *ReadyHandler {
+func NewReadyHandler(logger *zap.SugaredLogger, info *information.BotInformation) *ReadyHandler {
 	return &ReadyHandler{
-		logger:          logger,
-		versionResolver: resolver,
+		logger: logger,
+		info:   info,
 	}
 }
 
-func (h *ReadyHandler) Handle(session *discordgo.Session, event *discordgo.Ready) {
-	if err := session.UpdateCustomStatus("waking up"); err != nil {
-		h.logger.Errorf("failed to update custom status: %v", err)
-	}
+func (h *ReadyHandler) HandleReady(session *discordgo.Session, event *discordgo.Ready) {
 	h.logger.Infof("bot is ready as %s#%s", event.User.Username, event.User.Discriminator)
-	h.logger.Infof("version: %s", h.versionResolver.Version())
-	if err := session.UpdateCustomStatus(fmt.Sprintf("version : %s", h.versionResolver.Version())); err != nil {
-		h.logger.Errorf("failed to update custom status: %v", err)
-	}
+	h.logger.Infof("version: %s", h.info.Version)
 }
